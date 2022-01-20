b7fb673fd941a021dc6809fff098a6990f802e7b
Bruno Fernandes <bruno.fernandes3789@gmail.com>
2022-01-20 15:15:21 +0100

    new migration version

--- migrations/Version20220120141425.php
+++ migrations/Version20220120141425.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220120131902 extends AbstractMigration
+final class Version20220120141425 extends AbstractMigration
 {
     public function getDescription(): string
     {
